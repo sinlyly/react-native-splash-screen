e7e7d06aa8e06bdfc7e9a55df08891e982e45275
sin <379457721@qq.com>
2018-05-28 22:44:19 +0800

    override show method

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -14,20 +14,27 @@ import java.lang.ref.WeakReference;
  * Email:crazycodeboy@gmail.com
  */
 public class SplashScreen {
+    private static int NULL_ID = 0;
     private static Dialog mSplashDialog;
     private static WeakReference<Activity> mActivity;
 
     /**
      * 打开启动屏
      */
-    public static void show(final Activity activity, final int themeResId) {
+    public static void show(final Activity activity, final boolean fullScreen, final int themeResId) {
         if (activity == null) return;
         mActivity = new WeakReference<Activity>(activity);
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 if (!activity.isFinishing()) {
-                    mSplashDialog = new Dialog(activity, themeResId);
+
+                    mSplashDialog = new Dialog(
+                            activity,
+                            themeResId != NULL_ID ? themeResId
+                                    : fullScreen ? R.style.SplashScreen_Fullscreen
+                                    : R.style.SplashScreen_SplashTheme
+                    );
                     mSplashDialog.setContentView(R.layout.launch_screen);
                     mSplashDialog.setCancelable(false);
 
@@ -43,9 +50,7 @@ public class SplashScreen {
      * 打开启动屏
      */
     public static void show(final Activity activity, final boolean fullScreen) {
-        int resourceId = fullScreen ? R.style.SplashScreen_Fullscreen : R.style.SplashScreen_SplashTheme;
-
-        show(activity, resourceId);
+        show(activity, fullScreen, 0);
     }
 
     /**
